c6fd8c8182923d85487ae17f2fe14b0842dad892
André Castro <142420463+andrecastrom06@users.noreply.github.com>
2024-10-14 09:19:03 -0300

    Update TituloDivida.java

--- TituloDivida.java
+++ TituloDivida.java
@@ -1,7 +1,7 @@
 package br.com.cesarschool.poo.titulos.entidades;
 
 // Esta classe deve herdar de Ativo. E deve ter os seguintes atributos: taxaJuros, do tipo double. 
-// Deve ter um construtor público que inicializa os atributos, e métodos set/get públicos para os atributos. 
+// Deve ter um construtor público que inicializa os atributos, e métodos set/get públicos para os atributos (não read-only). 
 // Deve ter um método público double calcularPrecoTransacao(double montante): o preço da transação é montante vezes (1 - taxaJuros/100.0).
 
 import java.time.LocalDate;
