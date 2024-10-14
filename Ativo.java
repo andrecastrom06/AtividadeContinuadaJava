f6a3b4a41d3efb843aa251605ae791f40921f967
André Castro <142420463+andrecastrom06@users.noreply.github.com>
2024-10-14 09:15:57 -0300

    Update Ativo.java

--- Ativo.java
+++ Ativo.java
@@ -1,28 +1,39 @@
 package br.com.cesarschool.poo.titulos.entidades;
 
-// Esta classe deve herdar de Ativo. E deve ter os seguintes atributos: valorUnitario, do tipo double. 
+// Esta classe deve ter os seguintes atributos: identificador, do tipo int, nome, do tipo String, data de validade, do tipo LocalDate. 
 // Deve ter um construtor público que inicializa os atributos, e métodos set/get públicos para os atributos. 
-// Deve ter um método público double calcularPrecoTransacao(double montante): o preço da transação é o produto do montante pelo valorUnitario.
+// O atributo identificador é read-only fora da classe.
 
 import java.time.LocalDate;
 
-public class Acao extends Ativo {
-	private double valorUnitario;
-
-	public Acao(int identificador, String nome, LocalDate datadDeValidade, double valorUnitario) {
-		super(identificador, nome, datadDeValidade);
-		this.valorUnitario = valorUnitario;
-	}
-
-	public double getValorUnitario() {
-		return valorUnitario;
-	}
-
-	public void setValorUnitario(double valorUnitario) {
-		this.valorUnitario = valorUnitario;
-	}
+public class Ativo {
+	private int identificador;
+	private String nome;
+	private LocalDate dataDeValidade;
 	
-	public double calcularPrecoTransacao(double montante) {
-		return montante * valorUnitario;
+	public Ativo(int identificador, String nome, LocalDate dataDeValidade) {
+		this.identificador = identificador;
+		this.nome = nome;
+		this.dataDeValidade = dataDeValidade;
 	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataDeValidade() {
+		return dataDeValidade;
+	}
+
+	public void setDataDeValidade(LocalDate dataDeValidade) {
+		this.dataDeValidade = dataDeValidade;
+	}	
 }
