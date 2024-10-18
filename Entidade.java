8a4fdfd4fc7abcfa014c4565728f2df90b618b79
André Castro <142420463+andrecastrom06@users.noreply.github.com>
2024-10-18 14:58:00 -0300

    Update Entidade.java

--- Entidade.java
+++ Entidade.java
@@ -1,32 +1,51 @@
-package org.cesarschool.telas;
+package br.com.cesarschool.poo.titulos.entidades;
 
-public class Entidade {
-	private String codigo;
-	private String nome;
-	private double renda;
-	public Entidade(String codigo, String nome, double renda) {
-		super();
-		this.codigo = codigo;
-		this.nome = nome;
-		this.renda = renda;
-	}
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public double getRenda() {
-		return renda;
-	}
-	public void setRenda(double renda) {
-		this.renda = renda;
-	}
+// Esta classe deve ter os seguintes atributos: entidadeCredito, do tipo EntidadeOperadora, entidadeDebito, do tipo EntidadeOperadora, 
+// acao, do tipo Acao, tituloDivida, do tipo TituloDivida, valorOperacao, do tipo double, dataHoraOperacao, do tipo LocalDateTime. 
+// Deve ter um construtor público que inicializa os atributos. 
+// Deve ter métodos get/set públicos para todos os atributos, que são read-only fora da classe.
+
+import java.time.LocalDateTime;
+
+public class Transacao {
+	private EntidadeOperadora entidadeCredito;
+	private EntidadeOperadora entidadeDebito;
+	private Acao acao;
+	private TituloDivida tituloDivida;
+	private double valorOperacao;
+	private LocalDateTime dataHoraOperacao;
 	
+	public Transacao(EntidadeOperadora entidadeCredito, EntidadeOperadora entidadeDebito, Acao acao, TituloDivida tituloDivida, 
+			double valorOperacao, LocalDateTime dataHoraOperacao) {
+		this.entidadeCredito = entidadeCredito;
+		this.entidadeDebito = entidadeDebito;
+		this.acao = acao;
+		this.tituloDivida = tituloDivida;
+		this.valorOperacao = valorOperacao;
+		this.dataHoraOperacao = dataHoraOperacao;
+	}
+
+	public EntidadeOperadora getEntidadeCredito() {
+		return entidadeCredito;
+	}
+
+	public EntidadeOperadora getEntidadeDebito() {
+		return entidadeDebito;
+	}
+
+	public Acao getAcao() {
+		return acao;
+	}
+
+	public TituloDivida getTituloDivida() {
+		return tituloDivida;
+	}
+
+	public double getValorOperacao() {
+		return valorOperacao;
+	}
+
+	public LocalDateTime getDataHoraOperacao() {
+		return dataHoraOperacao;
+	}	
 }
