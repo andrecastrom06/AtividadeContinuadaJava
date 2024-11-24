e1c909eb6d61a00efc045550d7d5f61783339d87
LucasSukar <143460827+LucasSukar@users.noreply.github.com>
2024-11-24 13:19:01 -0300

    Update Acao.java

--- Acao.java
+++ Acao.java
@@ -1,28 +1,33 @@
 package br.com.cesarschool.poo.titulos.entidades;
 
-// Esta classe deve herdar de Ativo. E deve ter os seguintes atributos: valorUnitario, do tipo double. 
-// Deve ter um construtor público que inicializa os atributos, e métodos set/get públicos para os atributos. 
-// Deve ter um método público double calcularPrecoTransacao(double montante): o preço da transação é o produto do montante pelo valorUnitario.
-
 import java.time.LocalDate;
 
 public class Acao extends Ativo {
-	private double valorUnitario;
+    private static final long serialVersionUID = 1L;
+    private double valorUnitario;
 
-	public Acao(int identificador, String nome, LocalDate datadDeValidade, double valorUnitario) {
-		super(identificador, nome, datadDeValidade);
-		this.valorUnitario = valorUnitario;
-	}
+    public Acao(int identificador, String nome, LocalDate dataDeValidade, double valorUnitario) {
+        super(identificador, nome, dataDeValidade);
+        this.valorUnitario = valorUnitario;
+    }
 
-	public double getValorUnitario() {
-		return valorUnitario;
-	}
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
 
-	public void setValorUnitario(double valorUnitario) {
-		this.valorUnitario = valorUnitario;
-	}
-	
-	public double calcularPrecoTransacao(double montante) {
-		return montante * valorUnitario;
-	}
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double calcularPrecoTransacao(double montante) {
+        if (montante < 0) {
+            throw new IllegalArgumentException("Montante não pode ser negativo.");
+        }
+        return montante * valorUnitario;
+    }
+
+    @Override
+    public String getIdUnico() {
+        return String.valueOf(getIdentificador());
+    }
 }
